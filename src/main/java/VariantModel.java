5e7e93f00fb23610149d7b7ed359595e5004870c
Krzysztof Tomczak <krzysztof.g.tomczak@student.put.poznan.pl>
2017-05-08 22:04:05 +0200

    [KNN]Hu moments model added

--- src/main/java/VariantModel.java
+++ src/main/java/VariantModel.java
@@ -2,6 +2,8 @@ import org.opencv.core.Mat;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.imgproc.Moments;
 
 public enum VariantModel implements AbstractVariantModel {
 
@@ -12,6 +14,19 @@ public enum VariantModel implements AbstractVariantModel {
             // TODO Process extracted green colors and create a vector of attributes
             System.out.println("Debug");
             return new ArrayList<>();
+        }
+    },
+    HU_MOMENTS {
+        @Override
+        public List<Double> getVector(Mat image) {
+            final Moments moments = Imgproc.moments(image);
+            final Mat huMoments = new Mat();
+            Imgproc.HuMoments(moments, huMoments);
+            final List<Double> vector = new ArrayList<>();
+            for (int i = 0; i < 7; i++) {
+                vector.add(huMoments.get(i, 0)[0]);
+            }
+            return vector;
         }
     }
 }
