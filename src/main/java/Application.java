f5e2c4c9ccb49bdee9ca4131a472db58b831bcc9
Krzysztof Wencel <kwencel@users.noreply.github.com>
2017-05-21 14:15:03 +0200

    [Multithreading] First attempt - thread per image row.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,15 +1,16 @@
-import java.io.BufferedReader;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.imgcodecs.Imgcodecs;
+
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
-import org.opencv.core.Scalar;
-import org.opencv.imgcodecs.Imgcodecs;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Application {
 
@@ -21,38 +22,57 @@ public class Application {
     public static void main(String[] args) throws Exception {
         nu.pattern.OpenCV.loadShared();
         Application app = new Application();
-        app.run();
+        if (args.length != 2) {
+            System.err.println("Proper arguments: <dir containing 'images', 'manuals' and 'masks' subdirs> <image_to_classify>");
+            System.exit(1);
+        }
+        app.run(args);
     }
 
-    private void run() throws IOException {
-        BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Provide path to folder with learing examples.");
-        String line = buffer.readLine();
-        final Path samplesPath = Paths.get(line, "samples");
+    private void run(String[] args) throws IOException, InterruptedException {
+        String trainingDir = args[0];
+        String workingFile = args[1];
+        final Path samplesPath = Paths.get(trainingDir, "samples");
         final File samplesFolder = samplesPath.toFile();
         if (!samplesFolder.exists()) {
-            samplesFolder.mkdir();
-            ImageUtils.generateSamplesForFolder(line, samplesPath.toString(), NUMBER_OF_SAMPLES, NUMBER_OF_SAMPLES, SIZE);
+            boolean succeeded = samplesFolder.mkdir();
+            if (!succeeded) {
+                throw new RuntimeException("Could not create 'samples' directory");
+            }
+            ImageUtils.generateSamplesForFolder(trainingDir, samplesPath.toString(), NUMBER_OF_SAMPLES, NUMBER_OF_SAMPLES, SIZE);
         }
         final File[] samples = samplesFolder.listFiles();
         System.out.println(String.format("There are total of %1d samples of size %2d",
-                samples.length, SIZE * 2 + 1
+                                         samples.length, SIZE * 2 + 1
         ));
         final AbstractClassifier classifier = getTrainedClassifier(samples);
 
-        System.out.println("Provide path to image to classify.");
-        line = buffer.readLine();
-        final Mat image = Imgcodecs.imread(line);
+        final Mat image = Imgcodecs.imread(workingFile);
         final Mat result = new Mat(image.size(), image.type(), new Scalar(0));
-        for (int y = 0; y < image.height(); y++) {
-            for (int x = 0; x < image.width(); x++) {
-                final Mat surrounding = ImageUtils.getSurroundingPixels(image, x, y, SIZE);
-                if (classifier.isVessel(surrounding, surrounding.width() / 2, surrounding.height() / 2)) {
-                    result.put(y, x, 255, 255, 255);
-                }
+        ExecutorService exec = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        try {
+            for (int y = 0; y < image.height(); y++) {
+                final int finalY = y;
+                exec.submit(() -> {
+                    for (int x = 0; x < image.width(); x++) {
+                        final Mat surrounding = ImageUtils.getSurroundingPixels(image, x, finalY, SIZE);
+                        if (classifier.isVessel(surrounding, surrounding.width() / 2, surrounding.height() / 2)) {
+                            result.put(finalY, x, 255, 255, 255);
+                        }
+                    }
+                });
             }
+        } finally {
+            exec.shutdown();
+            // Wait forever for all threads
+            exec.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+            System.gc();
         }
-        Imgcodecs.imwrite(line + "_result", result);
+
+        String extension = workingFile.substring(workingFile.length() - 4);
+        String resultPath = workingFile.substring(0, workingFile.length() - 4) + "_result" + extension;
+        System.out.println(resultPath);
+        Imgcodecs.imwrite(resultPath, result);
     }
 
     private AbstractClassifier getTrainedClassifier(File[] samples) {
@@ -63,7 +83,6 @@ public class Application {
             vectors.add(variant.getTrainingVector(VARIANT_MODEL));
         }
         final NormalizedTrainingSet trainingSet = new NormalizedTrainingSet(vectors);
-        final AbstractClassifier classifier = new KNNClassifier(trainingSet, VARIANT_MODEL, NUMBER_OF_NEIGHBOURS);
-        return classifier;
+        return new KNNClassifier(trainingSet, VARIANT_MODEL, NUMBER_OF_NEIGHBOURS);
     }
 }
